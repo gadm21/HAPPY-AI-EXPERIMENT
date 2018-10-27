e0c7b3a971cfac77552555aea032e94657226885
Ubuntu <ubuntu@ip-172-31-2-56.ap-southeast-1.compute.internal>
2018-10-27 22:40:40 +0000

    [fix] port and deps

--- server/run.py
+++ server/run.py
@@ -3,4 +3,4 @@ import os
 from ai_api import app
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=8080, debug=os.environ.get('FLASK_DEBUG', False))
+    app.run(host= '0.0.0.0', port=5000, debug=os.environ.get('FLASK_DEBUG', False))
