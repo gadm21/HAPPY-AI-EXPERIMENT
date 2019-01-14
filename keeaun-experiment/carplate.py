579b10375c9b2c68780e6f5d385b836959c3c95b
Tapway <enquiry@gotapway.com>
2019-01-14 17:53:39 +0800

    Update carplate codes

--- keeaun-experiment/carplate.py
+++ keeaun-experiment/carplate.py
@@ -5,7 +5,8 @@ import sys
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--video", type=str, help="video path/ip address")
+    # add your video link in 'default' parameter
+    parser.add_argument("--video", default="video/vlc-record-2019-01-07-22h41m24s-Ainsdale-K09.mp4-.mp4", type=str, help="video path/ip address")
     parser.add_argument(
         "--width", default=0, type=int, help="the width of video output"
     )
@@ -13,7 +14,7 @@ def parse_arguments(argv):
         "--height", default=0, type=int, help="the height of video output"
     )
     parser.add_argument(
-        "--output", default="output/output.mov", type=str, help="output video path"
+        "--output", default="output/test.mov", type=str, help="output video path"
     )
     return parser.parse_args(argv)
 
@@ -28,7 +29,10 @@ if __name__ == "__main__":
 
     cap = cv2.VideoCapture(video_src)
 
-    resize = False
+    # resize video if required
+    resize = False # default is False
+    # args.width = 1024 # uncomment if resize is True
+    # args.height = 790 # uncomment if resize is True
 
     if args.width > 0:
         width = args.width
@@ -44,9 +48,6 @@ if __name__ == "__main__":
 
     output = args.output
 
-    frame_count = 0
-    frame_interval = 40
-
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     fps = cap.get(cv2.CAP_PROP_FPS)
 
@@ -55,6 +56,18 @@ if __name__ == "__main__":
     )
 
     carplate = None
+
+    # define what frame interval and crop region based on video width, height and fps
+    print(width, height, fps)
+
+    frame_count = 0
+    frame_interval = 10
+
+    # define your crop region here
+    x1 = 300
+    y1 = 350
+    x2 = 600
+    y2 = 500
 
     while True:
         flags, frame = cap.read()
@@ -66,7 +79,7 @@ if __name__ == "__main__":
             frame = cv2.resize(frame, (width, height))
 
         if (frame_count % frame_interval) == 0:
-            cropRegion = frame[319:492, 420:666]
+            cropRegion = frame[y1:y2, x1:x2]
 
             enc = cv2.imencode(".png", cropRegion)[1].tostring()
 
@@ -82,7 +95,7 @@ if __name__ == "__main__":
                 # print("Detected text:" + word)
                 if len(word) < 3:
                     continue
-                if len(word) > 6 and len(word) < 9:
+                if len(word) > 6 and len(word) < 9: #and response["Confidence"] > 60:
                     carplate = word
                     print(carplate)
                     break
@@ -102,7 +115,7 @@ if __name__ == "__main__":
         cv2.putText(
             frame, textT, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2
         )
-        cv2.rectangle(frame, (420, 319), (666, 492), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
         out.write(frame)
         cv2.imshow("Video", frame)
